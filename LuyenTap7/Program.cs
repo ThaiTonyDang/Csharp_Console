d67037d8530f31cd21f2f22b568f8d0804aa03c1
Thai Tran <dangtranthaidtv50dh@gmail.com>
2021-12-09 15:50:24 +0900

    add LuyenTap7

---
+++ LuyenTap7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LuyenTap7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3,-100, 11, 4,10, 5, 6, 7, 8 };
+           
+            int min, max;
+            max = array[0];
+            min = array[0];
+
+            for(int i = 0; i <=10 ; i++ )
+            {
+                if (max<array[i])
+                {
+                    max = array[i];
+                }
+                if(min>array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+            Console.WriteLine(" Result of max is " + max);
+            Console.WriteLine(" Result of min is " + min);
+            Console.ReadLine();
+        }
+    }
+}
